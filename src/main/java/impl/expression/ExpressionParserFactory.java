c5720e71a0a68fb2a5c97cff9663c4f6a0ba765b
Nikolay Rykunov <Nikolay.Rykunov@jetbrains.com>
2020-02-25 19:06:22 +0300

    [task-4] Introduce ExpressionParser interface and tests for its implementation

---
+++ src/main/java/impl/expression/ExpressionParserFactory.java
@@ -0,0 +1,9 @@
+package impl.expression;
+
+import api.expression.ExpressionParser;
+
+public class ExpressionParserFactory {
+    public static ExpressionParser createInstance() {
+        return null;
+    }
+}
