f0558a6aafd74ba6bc095d91577a4daddc90aa6b
Nikolay Rykunov <Nikolay.Rykunov@jetbrains.com>
2020-04-01 15:11:06 +0300

    [task-5] Introduce hw to work with files using different encodings

---
+++ src/main/java/impl/file/FileEncodingWriterFactory.java
@@ -0,0 +1,9 @@
+package impl.file;
+
+import api.file.FileEncodingWriter;
+
+public class FileEncodingWriterFactory {
+    public static FileEncodingWriter createInstance() {
+        return null;
+    }
+}
