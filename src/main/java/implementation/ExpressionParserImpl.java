cbab5114ba4af7c686d17dd0a108015ae7672fb9
Oleksiy Vovchenko <>
2021-08-11 19:39:52 +0300

    Second realization

--- src/main/java/implementation/ExpressionParserImpl.java
+++ src/main/java/implementation/ExpressionParserImpl.java
@@ -5,98 +5,89 @@ import api.expression.ParseException;
 
 public class ExpressionParserImpl implements ExpressionParser {
 
-    private Integer operand1 = null;
-    private Integer operand2 = null;
+    public enum OperatorType{
+        PLUS, MINUS
+    }
 
-    private Integer sum = 0;
+    private Integer operand1;
+    private Integer operand2;
 
-    private boolean operatorExists = false;
-    private String operator = "";
+    private Integer sum;
+
+    private boolean operatorExists;
+    private OperatorType operator;
 
     @Override
     public int parse(String expression) throws ParseException {
-
-        StringBuilder sb = new StringBuilder();
-        //sb.append()
-        //expression.charAt()
-
-
-
+        sum = 0;
+        operand1 = null;
+        operand2 = null;
+        operatorExists = false;
+        operator = null;
 
 
+        var sb = new StringBuilder();
+
+        for (int i = 0; i< expression.length(); i++){
+
+            var c = expression.charAt(i);
+
+            //if there is the ed of array string and no more numbers we won't enter the cycle
+            if(c == ' ') {
+                sb.append(expression.charAt(i));
+                sb.append(expression.charAt(i + 1));
+                sb.append(expression.charAt(i + 2));
+                if (sb.toString().equals(" + ")) {
+                    operator = OperatorType.PLUS;
+                    i += 2;
+                } else if (sb.toString().equals(" - ")) {
+                    operator = OperatorType.MINUS;
+                    i += 2;
+                } else {
+                    throw new ParseException("Wrong sequence");
+                }
+
+                sb.setLength(0);
+            }
+            else if( (c <= '9' && c >= '0') || c =='-'){
+
+                sb.append(c);
+
+                if(i + 1 < expression.length()){
+                    if(expression.charAt(i+1) == ' '){
+
+                        AddNewNumber(sb);
 
 
+                    }
+                }
+                else{
+
+                    AddNewNumber(sb);
+                }
 
 
+            }else{
+                throw new ParseException("Wrong sequence");
+            }
 
-        
-//        for (int i = 0; i < expression.length(); i++){
-//
-//            var symbol = expression.charAt(i);
-//
-//            switch (symbol){
-//                case '+':
-//                    if(expression.charAt(i - 1) == ' '){
-//                        operator = "+";
-//                    }
-//                    else{
-//                        throw new ParseException("Wrong sequence of numbers");
-//                    }
-//                    continue;
-//                case '-':
-//                    if(expression.charAt(i - 1) == ' '){
-//                        operator = "-";
-//                    }
-//                    else{
-//                        throw new ParseException("Wrong sequence of numbers");
-//                    }
-//                    continue;
-//                default:
-//                    if(symbol >= '1' && symbol <= '9'){
-//
-//                        sb.append(symbol);
-//
-//                    }else if(symbol == ' '){
-//
-//                        if(operand1 != null && operand2 != null && operatorExists){
-//
-//                            switch (operator){
-//                                case "+":
-//                                    sum +=
-//                            }
-//
-//                        }
-//
-//
-//                    }
-//
-//            }
-//
-//
-////            if(expression.charAt(i) != ' ' &&
-////                    (expression.charAt(i) <= '9' && expression.charAt(i) >= '1'
-////                            || expression.charAt(i) >= '-')){
-////
-////                sb.append(sb.append(expression.charAt(i)));
-////
-////            }else if(expression.charAt(i) == ' '){
-////
-////                if(operand1 == null){
-////                    operand1 = Integer.parseInt(sb.toString());
-////                }
-////                if(operand2 == null){
-////
-////                }
-////
-////
-////
-////            }else if( (expression.charAt(i) == '+' || expression.charAt(i+1) == '-'))
-//
-//        }
+        }
 
+        return sum;
+    }
 
+    private void AddNewNumber(StringBuilder sb) {
+        if(operator == null || operator == OperatorType.PLUS){
+            sum += Integer.parseInt(sb.toString());
 
+            sb.setLength(0);
+            operator = null;
+        } else if (operator == OperatorType.MINUS) {
+//if num > maxValue
+            sum -= Integer.parseInt(sb.toString());
 
-        return 0;
+            sb.setLength(0);
+            operator = null;
+        }
     }
 }
