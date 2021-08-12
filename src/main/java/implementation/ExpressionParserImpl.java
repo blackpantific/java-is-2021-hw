31cc60e334abe8b1095f5075827e560fb4ddb8cb
Oleksiy Vovchenko <>
2021-08-12 14:43:07 +0300

    Pre-final realization

--- src/main/java/implementation/ExpressionParserImpl.java
+++ src/main/java/implementation/ExpressionParserImpl.java
@@ -5,89 +5,113 @@ import api.expression.ParseException;
 
 public class ExpressionParserImpl implements ExpressionParser {
 
-    public enum OperatorType{
+    public enum OperatorType {
         PLUS, MINUS
     }
 
-    private Integer operand1;
-    private Integer operand2;
 
     private Integer sum;
+
+    private boolean operand1;
+    private boolean operand2;
 
     private boolean operatorExists;
     private OperatorType operator;
 
+
     @Override
     public int parse(String expression) throws ParseException {
-        sum = 0;
-        operand1 = null;
-        operand2 = null;
-        operatorExists = false;
-        operator = null;
 
+        InitializeValues();
 
-        var sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i< expression.length(); i++){
+        for (int i = 0; i < expression.length(); i++) {
 
             var c = expression.charAt(i);
 
-            //if there is the ed of array string and no more numbers we won't enter the cycle
-            if(c == ' ') {
-                sb.append(expression.charAt(i));
-                sb.append(expression.charAt(i + 1));
-                sb.append(expression.charAt(i + 2));
-                if (sb.toString().equals(" + ")) {
-                    operator = OperatorType.PLUS;
-                    i += 2;
-                } else if (sb.toString().equals(" - ")) {
-                    operator = OperatorType.MINUS;
-                    i += 2;
-                } else {
-                    throw new ParseException("Wrong sequence");
-                }
-
-                sb.setLength(0);
-            }
-            else if( (c <= '9' && c >= '0') || c =='-'){
-
-                sb.append(c);
-
-                if(i + 1 < expression.length()){
-                    if(expression.charAt(i+1) == ' '){
-
-                        AddNewNumber(sb);
-
-
+            switch (c) {
+                case '-':
+                    if (!operatorExists) {
+                        operator = OperatorType.MINUS;
+                        operatorExists = true;
+                        continue;
+                    } else {
+                        throw new ParseException("Wrong sequence!");
                     }
-                }
-                else{
+                case '+':
+                    if (!operatorExists) {
+                        operator = OperatorType.PLUS;
+                        operatorExists = true;
+                        continue;
+                    } else {
+                        throw new ParseException("Wrong sequence!");
+                    }
+                default:
+                    if (c <= '9' && c >= '0') {
 
-                    AddNewNumber(sb);
-                }
+                        sb.append(c);
 
+                        if ((i + 1) < expression.length()) {
+                            //do the same operations
 
-            }else{
-                throw new ParseException("Wrong sequence");
+                            if (expression.charAt(i + 1) > '9' || expression.charAt(i + 1) < '0') {
+
+                                AddNewNumber(sb);
+                            }
+
+                        } else {
+                            AddNewNumber(sb);
+                        }
+                        
+                    }
             }
 
         }
 
         return sum;
+
     }
 
-    private void AddNewNumber(StringBuilder sb) {
-        if(operator == null || operator == OperatorType.PLUS){
+    private void InitializeValues() {
+        sum = null;
+        operand1 = false;
+        operand2 = false;
+        operatorExists = false;
+        operator = null;
+    }
+
+    private void AddNewNumber(StringBuilder sb) throws ParseException {
+        if (operatorExists && sum == null) {
+
+            sum = 0;
+            if (operator == OperatorType.MINUS) {
+                sum -= Integer.parseInt(sb.toString());
+            } else {
+                sum += Integer.parseInt(sb.toString());
+            }
+
+        } else if (!operatorExists && sum == null) {
+
+            sum = 0;
             sum += Integer.parseInt(sb.toString());
 
-            sb.setLength(0);
-            operator = null;
-        } else if (operator == OperatorType.MINUS) {
-//if num > maxValue
-            sum -= Integer.parseInt(sb.toString());
+        } else if (operatorExists) {
 
-            sb.setLength(0);
-            operator = null;
+            if (operator == OperatorType.MINUS) {
+                sum -= Integer.parseInt(sb.toString());
+            } else {
+                sum += Integer.parseInt(sb.toString());
+            }
+
+        } else if (!operatorExists) {
+
+            throw new ParseException("Wrong sequence!");
         }
+
+        operator = null;
+        operatorExists = false;
+        sb.setLength(0);
     }
+
 }
